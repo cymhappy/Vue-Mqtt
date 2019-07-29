f0f8d3ec55e8b5cdcd050c1a529ff90a751587d8
Keith <yzhang@xinshiyun.com>
2019-07-30 01:01:28 +0800

    two mqtt clients

--- src/Main.js
+++ src/Main.js
@@ -3,45 +3,73 @@ import Emitter from './Emitter'
 
 export default {
 
-    install(Vue, connection, options) {
+    install(Vue, connection_signal, options_signal,connection_record,options_record) {
 
-        if (!connection) throw new Error("[Vue-Mqtt] cannot locate connection");
+        if (!connection_signal) throw new Error("[Vue-Mqtt] cannot locate connection_signal");
+        if (!connection_record) throw new Error("[Vue-Mqtt] cannot locate connection_record");
 
-        let observer = new Observer(connection, options);
+        let observer_signal = new Observer(connection_signal, options_signal);
+        let observer_record = new Observer(connection_record, options_record);
 
-        Vue.prototype.$mqtt = observer.Mqtt;
+        Vue.prototype.$mqtt_signal = observer_signal.Mqtt;
+        Vue.prototype.$mqtt_record = observer_record.Mqtt;
 
         Vue.mixin({
             created() {
-                let mqtt = this.$options['mqtt'];
-
-                this.$options.mqtt = new Proxy({}, {
+                let mqtt_signal = this.$options['mqtt_signal'];
+                this.$options.mqtt_signal = new Proxy({}, {
                     set: (target, key, value) => {
                         Emitter.addListener(key, value, this);
                         target[key] = value;
                         return true;
                     },
                     deleteProperty: (target, key) => {
-                        Emitter.removeListener(key, this.$options.mqtt[key], this);
+                        Emitter.removeListener(key, this.$options.mqtt_signal[key], this);
                         delete target.key;
                         return true;
                     }
                 });
-
-                if (mqtt) {
-                    Object.keys(mqtt).forEach((key) => {
-                        this.$options.mqtt[key] = mqtt[key];
+                if (mqtt_signal) {
+                    Object.keys(mqtt_signal).forEach((key) => {
+                        this.$options.mqtt_signal[key] = mqtt_signal[key];
                     });
                 }
+
+
+                let mqtt_record = this.$options['mqtt_record'];
+                this.$options.mqtt_record = new Proxy({}, {
+                    set: (target, key, value) => {
+                        Emitter.addListener(key, value, this);
+                        target[key] = value;
+                        return true;
+                    },
+                    deleteProperty: (target, key) => {
+                        Emitter.removeListener(key, this.$options.mqtt_record[key], this);
+                        delete target.key;
+                        return true;
+                    }
+                });
+                if (mqtt_record) {
+                    Object.keys(mqtt_record).forEach((key) => {
+                        this.$options.mqtt_record[key] = mqtt_record[key];
+                    });
+                } 
             },
             beforeDestroy() {
-                let mqtt = this.$options['mqtt'];
-
-                if (mqtt) {
-                    Object.keys(mqtt).forEach((key) => {
-                        delete this.$options.mqtt[key];
+                let mqtt_signal = this.$options['mqtt_signal'];
+                if (mqtt_signal) {
+                    Object.keys(mqtt_signal).forEach((key) => {
+                        delete this.$options.mqtt_signal[key];
                     });
                 }
+
+                
+                let mqtt_record = this.$options['mqtt_record'];
+                if (mqtt_record) {
+                    Object.keys(mqtt_record).forEach((key) => {
+                        delete this.$options.mqtt_record[key];
+                    });
+                }                
             }
         })
 
